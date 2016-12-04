bfcf6a1127d1ace1044c0599635e627551ffeaa6
Joshua <jross3740@gmail.com>
2016-12-04 13:28:00 -0500

    Update Main.php

--- src/OHKM/Main.php
+++ src/OHKM/Main.php
@@ -3,19 +3,25 @@ namespace OHKM;
 use pocketmine\Player;
 use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\entity\Creature;
+
 class Main extends PluginBase implements Listener{
-
-public function onEnable(){
-$this->getServer()->getPluginManager()->registerEvents($this, $this);
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onDamage(EntityDamageEvent $action){
+    if($action instanceof EntityDamageByEntityEvent){
+      $m = $action->getEntity();
+      $p = $action->getDamager();
+      if($m instanceof Creature){
+        $m->kill();
+      }
+    }
+  }
 }
-
-public function onDamage(EntityDamageByEntityEvent $action){
-$m = $action->getEntity();
-$p = $action->getDamager();
-if($m instanceof Creature){
-$m->kill();
-}
-}
+?>
